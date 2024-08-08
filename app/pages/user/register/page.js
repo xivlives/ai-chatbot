397351e0b166568f66a439d4457a41ee3df66a6b
xivlives <94845985+xivlives@users.noreply.github.com>
2024-08-08 13:19:43 +0100

    commit

---
+++ app/pages/user/register/page.js
@@ -0,0 +1,63 @@
+'use client'
+// pages/register.js
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/app/authContext';
+
+export default function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const  { register }  = useAuth();
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await register(name, email, password);
+      router.push('/pages/user/login');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md">
+        <h1 className="text-2xl mb-4">Register</h1>
+        {/* Name Input */}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Name
+          </label>
+          <input
+            id="name"
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your name"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        {/* email Input */}
+        <input
+          type="email"
+          className="block border border-grey-light w-full p-3 text-gray-700 rounded mb-4"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {/* password Input */}
+        <input
+          type="password"
+          className="block border border-grey-light w-full p-3 text-gray-700 rounded mb-4"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" className="w-full text-center py-3 rounded bg-blue-500 text-white hover:bg-blue-700">Register</button>
+      </form>
+    </div>
+  );
+}
